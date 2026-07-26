6caac12cd7f9e967ab6955cd634f00084d703594
nuxtgen-frontend <jshahadat8@gmail.com>
2026-07-26 16:25:13 +0600

    build:

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,18 @@
-import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import express from "express";
 import authRoutes from "./routes/auth.js";
 import jobsRoutes from "./routes/jobs.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://job-tarcker-fv6xzwjrk-jshahadats-projects.vercel.app",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
